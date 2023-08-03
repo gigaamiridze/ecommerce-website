9edad04c5e531509a95ce73502d4cb835615a43b
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-03 11:06:50 +0400

    Added client feature to get all products

---
+++ client/src/constants/ApiRoutes.ts
@@ -0,0 +1,4 @@
+export enum ApiRoutes {
+  HOST = 'http://127.0.0.1:8000/api',
+  PRODUCTS = `${HOST}/products`,
+}
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,22 @@
-import { products } from '../data';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { IProduct } from '../interfaces';
+import { ApiRoutes } from '../constants';
 import { ProductItem } from '../layouts';
 import { HomeTitle, ProductsList } from '../components';
 
 function Home() {
+  const [products, setProducts] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    const { data } = await axios.get(ApiRoutes.PRODUCTS);
+    setProducts(data);
+  }
+
   return (
     <>
       <HomeTitle>latest products</HomeTitle>
