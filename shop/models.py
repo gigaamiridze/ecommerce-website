cea54a0e7827621116efc70695ff29c8a2202215
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-03 13:13:56 +0400

    Added review model

--- shop/models.py
+++ shop/models.py
@@ -17,3 +17,14 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Review(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    rating = models.IntegerField(null=True, blank=True, default=0)
+    comment = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.rating)
