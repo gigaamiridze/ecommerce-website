b14f6aabb0f41b33388a10b030a658698c76e905
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-03 09:04:23 +0400

    Added get routes view

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def get_routes(request):
+    routes = [
+        {
+            'Endpoint': '/products',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of products'
+        },
+        {
+            'Endpoint': '/products/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single product object'
+        },
+        {
+            'Endpoint': '/products',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates new product with data sent in post request'
+        },
+        {
+            'Endpoint': '/products/id',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Creates an existing product with data sent in post request'
+        },
+        {
+            'Endpoint': '/products/id',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes and exiting product'
+        },
+    ]
+
+    return JsonResponse(routes, safe=False)
