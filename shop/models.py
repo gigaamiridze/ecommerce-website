04ed2ae32a5beb3b1dd187982325f991c120686c
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-03 13:43:42 +0400

    Added order model

--- shop/models.py
+++ shop/models.py
@@ -11,9 +11,9 @@ class Product(models.Model):
     category = models.CharField(max_length=200, null=True, blank=True)
     price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
-    numReviews = models.IntegerField(null=True, blank=True, default=0)
-    countInStock = models.IntegerField(null=True, blank=True, default=0)
-    createdAt = models.DateTimeField(auto_now_add=True)
+    num_reviews = models.IntegerField(null=True, blank=True, default=0)
+    count_in_stock = models.IntegerField(null=True, blank=True, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
@@ -28,3 +28,19 @@ class Review(models.Model):
 
     def __str__(self):
         return str(self.rating)
+
+class Order(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    payment_method = models.CharField(max_length=200, null=True, blank=True)
+    tax_price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    shipping_price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    total_price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    is_paid = models.BooleanField(default=False)
+    paid_at = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    is_delivered = models.BooleanField(default=False)
+    delivered_at = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.created_at)
