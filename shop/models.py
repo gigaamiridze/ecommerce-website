a7114deea2a31d7bde906b4a9b745ff3e6eee548
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-20 13:27:38 +0400

    Modified models

--- shop/models.py
+++ shop/models.py
@@ -1,14 +1,15 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 from django.contrib.auth.models import User
 
 class Product(models.Model):
     _id = models.AutoField(primary_key=True, editable=False)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    name = models.CharField(max_length=200, null=True, blank=True)
+    name = models.CharField(max_length=100, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    description = models.TextField(null=True, blank=True)
-    brand = models.CharField(max_length=200, null=True, blank=True)
-    category = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(validators=[MinLengthValidator(10)], null=True, blank=True)
+    brand = models.CharField(max_length=100, null=True, blank=True)
+    category = models.CharField(max_length=100, null=True, blank=True)
     price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     num_reviews = models.IntegerField(null=True, blank=True, default=0)
@@ -22,9 +23,9 @@ class Review(models.Model):
     _id = models.AutoField(primary_key=True, editable=False)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
-    name = models.CharField(max_length=200, null=True, blank=True)
+    name = models.CharField(max_length=100, null=True, blank=True)
     rating = models.IntegerField(null=True, blank=True, default=0)
-    comment = models.TextField(null=True, blank=True)
+    comment = models.TextField(validators=[MinLengthValidator(4)], null=True, blank=True)
 
     def __str__(self):
         return str(self.rating)
@@ -32,7 +33,7 @@ class Review(models.Model):
 class Order(models.Model):
     _id = models.AutoField(primary_key=True, editable=False)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    payment_method = models.CharField(max_length=200, null=True, blank=True)
+    payment_method = models.CharField(max_length=100, null=True, blank=True)
     tax_price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     shipping_price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     total_price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
@@ -49,10 +50,10 @@ class OrderItem(models.Model):
     _id = models.AutoField(primary_key=True, editable=False)
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
-    name = models.CharField(max_length=200, null=True, blank=True)
+    name = models.CharField(max_length=100, null=True, blank=True)
     quantity = models.IntegerField(null=True, blank=True, default=0)
     price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
-    image = models.CharField(max_length=200, null=True, blank=True)
+    image = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return str(self.name)
@@ -60,10 +61,10 @@ class OrderItem(models.Model):
 class ShippingAddress(models.Model):
     _id = models.AutoField(primary_key=True, editable=False)
     order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True)
-    address = models.CharField(max_length=200, null=True, blank=True)
-    city = models.CharField(max_length=200, null=True, blank=True)
-    postal_code = models.CharField(max_length=200, null=True, blank=True)
-    country = models.CharField(max_length=200, null=True, blank=True)
+    address = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    postal_code = models.CharField(max_length=100, null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
     shipping_price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
 
     def __str__(self):
