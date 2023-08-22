d51014835343e7c64346d0770cab0e531842ea4d
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-22 10:41:36 +0400

    JSON Web Token Authentication has been setup

--- shop/urls.py
+++ shop/urls.py
@@ -1,3 +1,4 @@
+from rest_framework_simplejwt.views import TokenObtainPairView
 from django.urls import path
 from . import views
 
@@ -5,4 +6,5 @@ urlpatterns = [
     path('', views.get_routes, name='routes'),
     path('products', views.get_products, name='products'),
     path('products/<str:pk>', views.get_product, name='product'),
+    path('auth/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
